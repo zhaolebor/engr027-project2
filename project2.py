af62a18873d9dec3d24a85153b04512201419859
zzhao1 <ALAN@Alans-MacBook-Air.local>
2017-02-26 15:52:47 -0500

    almost

--- project2.py
+++ project2.py
@@ -19,6 +19,7 @@ import sys
 def fixKeyCode(code):
     return numpy.uint8(code).view(numpy.int8)
 
+# function to build up a Lapacian pyramid with an image input
 def pyr_build(img):
     G = []
     L = []
@@ -42,6 +43,7 @@ def pyr_build(img):
     L[-1] = G[-2]
     return L
 
+# function to reconstruct an image using a Laplacian pyramid as an inpu
 def pyr_reconstruct(L):
     n = len(L)-1
     R = []
@@ -51,19 +53,14 @@ def pyr_reconstruct(L):
         w = L[i-1].shape[1]
         h = L[i-1].shape[0]
         r_up = cv2.pyrUp(R[n-i], dstsize = (w, h))
-        #We don't need to convert to float32 because they already were
-        #r_up_32 = numpy.array(r_up, dtype = 'float32')
-        #l_32 = numpy.array(L[i-1], dtype = 'float32')
         new_r = r_up + L[i-1]
         R.append(new_r)
         i -= 1
 
     return R[n]
 
+# function to alpha blend two images of the same size
 def alpha_blend(A, B, alpha):
-    #print(A.shape)
-    #print(B.shape)
-    #print(alpha.shape)
     A = A.astype(alpha.dtype)
     B = B.astype(alpha.dtype)
     # if A and B are RGB images, we must pad
@@ -72,6 +69,7 @@ def alpha_blend(A, B, alpha):
         alpha = numpy.expand_dims(alpha, 2)
     return A + alpha*(B-A)
 
+# function to build up two pyramids for two images, alpha blend each layer of the pyramids and reconstruct a new image
 def LP_alpha_blend(A, B, alpha):
     lp_a = pyr_build(A)
     lp_b = pyr_build(B)
@@ -85,6 +83,7 @@ def LP_alpha_blend(A, B, alpha):
     result = pyr_reconstruct(blend)
     return result
 
+# function to align image B with the size of image A
 def align(A, B):
     w_a = A.shape[1]
     h_a = A.shape[0]
@@ -98,6 +97,7 @@ def align(A, B):
 
 
 ########################################################################
+# pyramid functionality
 if sys.argv[1] == 'pyramid':
     if len(sys.argv) != 3:
         print('usage: {} pyramid IMAGE1'.format(sys.argv[0]))
@@ -110,6 +110,7 @@ if sys.argv[1] == 'pyramid':
 
     lp = pyr_build(original)
 
+    # display each layer of the pyramid
     counter = 0
     for item in lp:
         cv2.imshow('pyramid', 0.5 + 0.5*(item / numpy.abs(item).max()))
@@ -118,11 +119,13 @@ if sys.argv[1] == 'pyramid':
         while fixKeyCode(cv2.waitKey(15)) < 0:
             pass
 
+    # reconstruct the original image using the pyramid generated above
     convert = pyr_reconstruct(lp)
     convert_clip = numpy.clip(convert, 0, 255)
     convert_8 = numpy.array(convert_clip, dtype = 'uint8')
     cv2.imshow('convert', convert_8)
 
+# blend functionality
 elif sys.argv[1] == 'blend':
     if len(sys.argv) != 4:
         print('usage: {} blend IMAGE1 IMAGE2'.format(sys.argv[0]))
@@ -137,34 +140,56 @@ elif sys.argv[1] == 'blend':
 
     width = A.shape[1]
     height = A.shape[0]
-    cx = width/2
-    cy = height/2
-    angle = 0
-    sigma = 0
 
+    # prompt user for parameters of the alpha mask
+    cx = int(raw_input('Enter the x-coord of the mask(-1 for default: center of the image): '))
+    cy = int(raw_input('Enter the y-coord of the mask(-1 for default: center of the image): '))
+    ellipse_width = int(raw_input('Enter the width of the ellipse(-1 for default: 1/4 of the image width): '))
+    ellipse_height = int(raw_input('Enter the height of the ellipse(-1 for default: 1/4 of the image height): '))
+    angle = int(raw_input('Enter the angle of the ellipse: '))
+    sigma = int(raw_input('Enter the sigma of the Gaussian blur: '))
+    ksize = int(raw_input('Enter the size of the Gaussian kernel: '))
+
+    if cx == -1:
+        cx = width/2
+    if cy == -1:
+        cy = height/2
+    if ellipse_width == -1:
+        ellipse_width = width/4
+    if ellipse_height == -1:
+        ellipse_height = height/4
+
+    # generate alpha mask based on the specified parameters
     mask = numpy.zeros((height, width), dtype=numpy.uint8)
-    cv2.ellipse(mask, (cx, cy), (width/4, height/4), angle, 0, 360, (255, 255, 255), -1, cv2.LINE_AA)
-    mask_blurred = cv2.GaussianBlur(mask, (5,5), sigma)
+    cv2.ellipse(mask, (cx, cy), (ellipse_width, ellipse_height), angle, 0, 360, (255, 255, 255), -1, cv2.LINE_AA)
+    mask_blurred = cv2.GaussianBlur(mask, (ksize,ksize), sigma)
     alpha = mask.astype(numpy.float32) / 255.0
 
-    cv2.namedWindow('alpha')
-    cv2.imshow('alpha', alpha)
+    cv2.namedWindow('A')
+    cv2.imshow('A', A)
+    cv2.namedWindow('B')
+    cv2.imshow('B', B)
 
+    #cv2.namedWindow('alpha')
+    #cv2.imshow('alpha', alpha)
+
+    # directly alpha blend the two images
     blend = alpha_blend(A, B, alpha)
-
     blend_clip = numpy.clip(blend, 0, 255)
     blend_8 = numpy.array(blend_clip, dtype = 'uint8')
     cv2.namedWindow('blend')
     cv2.imshow('blend', blend_8)
+    cv2.imwrite('blend.jpg', blend_8)
 
+    # pyramid alpha blend the two images
     pyramid_blend = LP_alpha_blend(A, B, alpha)
-
     pyramid_blend_clip = numpy.clip(pyramid_blend, 0, 255)
     pyramid_blend_8 = numpy.array(pyramid_blend_clip, dtype = 'uint8')
     cv2.namedWindow('pyramid blend')
     cv2.imshow('pyramid blend', pyramid_blend_8)
+    cv2.imwrite('pyramid_blend.jpg', pyramid_blend_8)
 
-
+# hybrid functionality
 elif sys.argv[1] == 'hybrid':
     if len(sys.argv) != 4:
         print('usage: {} blend IMAGE1 IMAGE2'.format(sys.argv[0]))
@@ -177,21 +202,31 @@ elif sys.argv[1] == 'hybrid':
 
     B = align(A, B)
 
-    k_a = 1
-    k_b = 1
-    sigma_a = 10
-    sigma_b = 5
+    # tested parameters that worked well
+    k_a = 0.8
+    k_b = 1.2
+    sigma_a = 50
+    sigma_b = 30
+    size_a = 55
+    size_b = 15
 
-    A_low = cv2.GaussianBlur(A, (5,5), sigma_a).astype('float32')
-    B_low = cv2.GaussianBlur(B, (5,5), sigma_b).astype('float32')
+    # generate low-pass filter for image A and high-pass filter for image B
+    A_low = cv2.GaussianBlur(A, (size_a,size_a), sigma_a).astype('float32')
+    B_low = cv2.GaussianBlur(B, (size_b,size_b), sigma_b).astype('float32')
     B_high = B - B_low
 
     I = k_a * A_low + k_b * B_high
 
+    # display the original images and save the new image
     I_clip = numpy.clip(I, 0, 255)
     I_8 = numpy.array(I_clip, dtype = 'uint8')
+    cv2.namedWindow('A')
+    cv2.imshow('A', A)
+    cv2.namedWindow('B')
+    cv2.imshow('B', B)
     cv2.namedWindow('hybrid')
     cv2.imshow('hybrid', I_8)
+    cv2.imwrite('hybrid.jpg', I_8)
 
 
 else:
@@ -200,9 +235,6 @@ else:
     print('usage: {} hybrid IMAGE1 IMAGE2'.format(sys.argv[0]))
 
     sys.exit(1)
-
-
-
 
 
 while fixKeyCode(cv2.waitKey(15)) < 0:
